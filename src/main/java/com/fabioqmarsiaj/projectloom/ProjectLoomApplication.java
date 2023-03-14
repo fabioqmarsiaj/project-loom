f378c0582cf2b08c51b87632595423ad65284c7b
fabioqmarsiaj <fabiobackend@gmail.com>
2023-03-14 08:32:31 -0300

    first commit

---
+++ src/main/java/com/fabioqmarsiaj/projectloom/ProjectLoomApplication.java
@@ -0,0 +1,13 @@
+package com.fabioqmarsiaj.projectloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectLoomApplication.class, args);
+    }
+
+}
