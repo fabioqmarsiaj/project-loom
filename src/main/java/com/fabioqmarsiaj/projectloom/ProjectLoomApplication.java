ac769ab3cf64f5b43a4260aee4b555ed0224c000
fabioqmarsiaj <fabiobackend@gmail.com>
2023-03-14 08:41:42 -0300

    Virtual threads

--- src/main/java/com/fabioqmarsiaj/projectloom/ProjectLoomApplication.java
+++ src/main/java/com/fabioqmarsiaj/projectloom/ProjectLoomApplication.java
@@ -9,6 +9,7 @@ public class ProjectLoomApplication {
     public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(ProjectLoomApplication.class, args);
 
+        // * Virtual threads are cheap enough that a new virtual thread can be created for each task, there should never be a need to pool virtual threads.
         Thread thread = Thread.ofVirtual().start(() -> System.out.println("Hello"));
         thread.join();
     }
