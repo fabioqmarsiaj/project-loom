52eab7a85900a85ba5059fd5f451aab60129b267
fabioqmarsiaj <fabiobackend@gmail.com>
2023-03-14 08:39:29 -0300

    Enabling java 19 preview features

--- src/main/java/com/fabioqmarsiaj/projectloom/ProjectLoomApplication.java
+++ src/main/java/com/fabioqmarsiaj/projectloom/ProjectLoomApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectLoomApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(ProjectLoomApplication.class, args);
+
+        Thread thread = Thread.ofVirtual().start(() -> System.out.println("Hello"));
+        thread.join();
     }
 
 }
